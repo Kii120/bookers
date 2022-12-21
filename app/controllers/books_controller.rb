1888b216e5facfb9d9eeddfd0f1467a34cccc631
EC2 Default User <ec2-user@ip-172-31-4-33.ec2.internal>
2022-12-21 14:25:40 +0000

    [Finish] assignment

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,21 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+  # def new
+  #   @book = Book.new
+  # end
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    flash[:notice] = "Book was succesfully created."
-    redirect_to book_path(book.id)
-    # else
-    #   render :new
-    # end
+    @books = Book.all
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -26,9 +28,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
+# class PostCodeValidator < ActiveModel::EachValidator
+#   def validate_each(record, attribute, value)
+#     record.errors.add(attribute, "can't be blank") if attribute.blank?
+#   end
+# end
+
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
 end
